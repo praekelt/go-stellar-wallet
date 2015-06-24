99a494986a3ef042abb9d0324d93c015bdb51a7a
Yaseen Hamdulay <yaseen@hamdulay.co.za>
2015-06-24 05:05:54 -0400

    convert tabs to spaces

--- src/controllers/wallet.js
+++ src/controllers/wallet.js
@@ -3,10 +3,10 @@ var Validator = require('validator');
 var Restify = require('restify');
 
 var wallet = {
-	create: function(req, res, next) {
+    create: function(req, res, next) {
         // NOTE: request must have content-type set application/json 
         // or application/x-www-form-urlencoded
-		var error_message = '', msisdn, pin;
+        var error_message = '', msisdn, pin;
         if (req.params) {
             msisdn = req.params['msisdn'];
             pin = req.params['pin'];
@@ -15,24 +15,24 @@ var wallet = {
         if (Object.keys(req.params).length === 0) {
             error_message = 'Please set content-type header approporiately';
         }
-		else if (pin === undefined ||
-		    msisdn === undefined) {
-			error_message = 'missing argument';
-		}
-		// TODO: this shouldn't just be ZA
-		else if (!Validator.isMobilePhone(msisdn, 'en-ZA')) {
-			error_message = 'not a validate mobile number';
-		}
-		else if (!Validator.isNumeric(pin)) {
-			error_message = 'pin must be numeric';
-		}
+        else if (pin === undefined ||
+            msisdn === undefined) {
+            error_message = 'missing argument';
+        }
+        // TODO: this shouldn't just be ZA
+        else if (!Validator.isMobilePhone(msisdn, 'en-ZA')) {
+            error_message = 'not a validate mobile number';
+        }
+        else if (!Validator.isNumeric(pin)) {
+            error_message = 'pin must be numeric';
+        }
         else if(pin.length < 5) {
             error_message = 'pin too short, must be at least 5 digits';
         }
 
-		if (error_message != '') {
-			return next(new Restify.BadRequestError(error_message));
-		}
+        if (error_message != '') {
+            return next(new Restify.BadRequestError(error_message));
+        }
 
         var createWalletPromise = WalletModel.create(msisdn, pin)
             .done(function(result) {
@@ -41,6 +41,6 @@ var wallet = {
                 next();
             });
         
-	},
+    },
 };
 module.exports = wallet;
--- src/wallet.js
+++ src/wallet.js
@@ -2,39 +2,39 @@ var WalletModel = require('./models/wallet');
 var Validator = require('validator');
 
 var wallet = {
-	create: function(req, res, next) {
-		// parse arguments
-		// call create wallet on wallet server
-		var msisdn = req.params['msisdn'];
-		var pin = req.params['pin'];
-		var verify_pin = req.params['verify-pin'];
-		
-		var error_message = '';
-		if (pin !== undefined &&
-		    verify_pin !== undefined &&
-		    msisdn !== undefined) {
-			error_message = 'missing argument';
-		}
-		// TODO: this shouldn't just be ZA
-		if (!validate.isMobilePhone(msisdn, 'en-ZA')) {
-			error_message = 'not a validate mobile number';
-		}
-		if (!validate.isNumeric(pin)) {
-			error_message = 'pin must be numeric';
-		}
-		if (pin != verify_pin) {
-			error_message = 'pins do not match';
-		}
+    create: function(req, res, next) {
+        // parse arguments
+        // call create wallet on wallet server
+        var msisdn = req.params['msisdn'];
+        var pin = req.params['pin'];
+        var verify_pin = req.params['verify-pin'];
+        
+        var error_message = '';
+        if (pin !== undefined &&
+            verify_pin !== undefined &&
+            msisdn !== undefined) {
+            error_message = 'missing argument';
+        }
+        // TODO: this shouldn't just be ZA
+        if (!validate.isMobilePhone(msisdn, 'en-ZA')) {
+            error_message = 'not a validate mobile number';
+        }
+        if (!validate.isNumeric(pin)) {
+            error_message = 'pin must be numeric';
+        }
+        if (pin != verify_pin) {
+            error_message = 'pins do not match';
+        }
 
-		if (error_message != '') {
-			return next(new restify.BadRequestError(error_message));
-		}
+        if (error_message != '') {
+            return next(new restify.BadRequestError(error_message));
+        }
 
-		next();
-	},
+        next();
+    },
 
-	fetch: function(req, res, next) {
-	}
+    fetch: function(req, res, next) {
+    }
 };
 
 module.exports = wallet;
